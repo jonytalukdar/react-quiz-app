424dc525ac008c0413399b4dfc16e00c7a6461da
jonytalukdar <jonytalukdar30@gmail.com>
2021-09-07 13:10:17 +0600

    create context

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,22 @@ import Quiz from './Pages/Quiz';
 import Result from './Pages/Result';
 import Signup from './Pages/Signup';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AuthContextProvider from '../contexts/AuthContext';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/quiz" component={Quiz} />
-          <Route path="/result" component={Result} />
-        </Switch>
-      </Layout>
+      <AuthContextProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/quiz" component={Quiz} />
+            <Route path="/result" component={Result} />
+          </Switch>
+        </Layout>
+      </AuthContextProvider>
     </Router>
   );
 }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,15 +1,58 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import '../firebase.config';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {
-  const singup = () => {
-    console.log('signing');
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const clear = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+    return clear;
+  }, []);
+
+  //   signup function
+  const singup = async (username, email, password) => {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    // update profile
+    await updateProfile(auth.user, {
+      displayName: username,
+    });
+
+    const loginUser = auth.user;
+    setUser({ ...loginUser });
+  };
+
+  //   login function
+  const login = (email, password) => {
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // logout function
+  const logout = () => {
+    const auth = getAuth();
+    return signOut(auth);
   };
 
   return (
-    <AuthContext.Provider value={{ singup }}>
-      {props.children}
+    <AuthContext.Provider value={{ user, singup, login, logout }}>
+      {!isLoading && props.children}
     </AuthContext.Provider>
   );
 };
